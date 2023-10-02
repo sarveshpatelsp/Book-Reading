024b34bfbccc614d14bbeb31c07e86eb63205c75
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2023-10-02 20:25:08 +0530

    Modified ModelClass.java and MainActivity.java

--- app/src/main/java/com/example/bookreadingapp/Adapter.java
+++ app/src/main/java/com/example/bookreadingapp/Adapter.java
@@ -1,48 +1,36 @@
 package com.example.bookreadingapp;
 
-import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
-
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private final ArrayList<ModelClass> arrayList;
-    private final Context context;
-    public Adapter(ArrayList<ModelClass> arrayList, Context context) {
-        this.arrayList = arrayList;
-        this.context = context;
+
+    public Adapter(ArrayList<ModelClass> bookInfoArrayList) {
+        this.arrayList = bookInfoArrayList;
     }
 
     @NonNull
     @Override
-    public Adapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_card , parent , false);
-        return new viewHolder(view);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_card, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter.viewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ModelClass modelClass = arrayList.get(position);
-        holder.titleCard.setText(modelClass.getTitle());
-        holder.publisherCard.setText(modelClass.getPublisher());
-        holder.pageCountCard.setText("No. of pages = " + modelClass.getPageCount());
-        Picasso.get().load(modelClass.getThumbnail()).into(holder.imageViewCard);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context , "CLicked" , Toast.LENGTH_LONG).show();
-            }
-        });
+        holder.title.setText(modelClass.getTitle());
+        holder.publisher.setText(modelClass.getPublisher());
+        holder.pageCount.setText("No of Pages : " + modelClass.getPageCount());
+        Picasso.get().load(modelClass.getThumbnail()).into(holder.imageView);
     }
 
     @Override
@@ -50,15 +38,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
         return arrayList.size();
     }
 
-    public class viewHolder extends RecyclerView.ViewHolder {
-        TextView titleCard, publisherCard, pageCountCard;
-        ImageView imageViewCard;
-        public viewHolder(@NonNull View itemView) {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title, publisher, pageCount;
+        ImageView imageView;
+
+        public ViewHolder(View itemView) {
             super(itemView);
-            titleCard = itemView.findViewById(R.id.card_title_text);
-            publisherCard = itemView.findViewById(R.id.card_publisher_text);
-            pageCountCard = itemView.findViewById(R.id.card_page_count_text);
-            imageViewCard = itemView.findViewById(R.id.image_view);
+            title = itemView.findViewById(R.id.card_title_text);
+            publisher = itemView.findViewById(R.id.card_publisher_text);
+            pageCount = itemView.findViewById(R.id.card_page_count_text);
+            imageView = itemView.findViewById(R.id.card_image_view);
         }
     }
 }
--- app/src/main/java/com/example/bookreadingapp/ModelClass.java
+++ app/src/main/java/com/example/bookreadingapp/ModelClass.java
@@ -1,6 +1,7 @@
 package com.example.bookreadingapp;
 
 import java.util.ArrayList;
+
 public class ModelClass {
 
     private String title;
@@ -18,72 +19,94 @@ public class ModelClass {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getSubtitle() {
         return subtitle;
     }
+
     public void setSubtitle(String subtitle) {
         this.subtitle = subtitle;
     }
+
     public ArrayList<String> getAuthors() {
         return authors;
     }
+
     public void setAuthors(ArrayList<String> authors) {
         this.authors = authors;
     }
+
     public String getPublisher() {
         return publisher;
     }
+
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
+
     public String getPublishedDate() {
         return publishedDate;
     }
+
     public void setPublishedDate(String publishedDate) {
         this.publishedDate = publishedDate;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public int getPageCount() {
         return pageCount;
     }
+
     public void setPageCount(int pageCount) {
         this.pageCount = pageCount;
     }
+
     public String getThumbnail() {
         return thumbnail;
     }
+
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
     }
+
     public String getPreviewLink() {
         return previewLink;
     }
+
     public void setPreviewLink(String previewLink) {
         this.previewLink = previewLink;
     }
+
     public String getInfoLink() {
         return infoLink;
     }
+
     public void setInfoLink(String infoLink) {
         this.infoLink = infoLink;
     }
+
     public String getBuyLink() {
         return buyLink;
     }
+
     public void setBuyLink(String buyLink) {
         this.buyLink = buyLink;
     }
+
     public ModelClass(String title, String subtitle, ArrayList<String> authors, String publisher,
-                    String publishedDate, String description, int pageCount, String thumbnail,
-                    String previewLink, String infoLink, String buyLink) {
+                      String publishedDate, String description, int pageCount, String thumbnail,
+                      String previewLink, String infoLink, String buyLink) {
         this.title = title;
         this.subtitle = subtitle;
         this.authors = authors;
